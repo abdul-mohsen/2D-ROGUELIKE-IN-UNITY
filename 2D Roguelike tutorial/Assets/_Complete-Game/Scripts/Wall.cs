1d0eb8a0a089ca2b7b0beacfcb1a4de9e5d6648f
abdul-mohsen <46282360+abdul-mohsen@users.noreply.github.com>
2019-04-28 06:58:05 +0300

    finished the tutorial
    Finished the tutorial
    + Changed the size to fit the phone
    + Changed the behavior of the enemy
    + Adjusted the damage and health for all object
    + Added more radomizion

---
+++ 2D Roguelike tutorial/Assets/_Complete-Game/Scripts/Wall.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class Wall : MonoBehaviour
+{
+    //public AudioClip chopSound1;                //1 of 2 audio clips that play when the wall is attacked by the player.
+    //public AudioClip chopSound2;                //2 of 2 audio clips that play when the wall is attacked by the player.
+    public Sprite dmgSprite;                    //Alternate sprite to display after Wall has been attacked by player.
+    public int hp = 3;                          //hit points for the wall.
+    public AudioClip chop1Sound;
+    public AudioClip chop2Sound;
+
+
+    private SpriteRenderer spriteRenderer;      //Store a component reference to the attached SpriteRenderer.
+
+
+    void Awake()
+    {
+        //Get a component reference to the SpriteRenderer.
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    //DamageWall is called when the player attacks a wall.
+    public void DamageWall(int dmg)
+    {
+        //Call the RandomizeSfx function of SoundManager to play one of two chop sounds.
+        SoundManager.instance.RandomizeSfx(chop1Sound, chop2Sound);
+
+        //Set spriteRenderer to the damaged wall sprite.
+        spriteRenderer.sprite = dmgSprite;
+
+        //Randomize the dmg
+        int loss = Random.Range(1,dmg+1);
+
+        //Subtract loss from hit point total.
+        hp -= loss;
+
+        //If hit points are less than or equal to zero:
+        if (hp <= 0)
+            //Disable the gameObject.
+            gameObject.SetActive(false);
+    }
+}
